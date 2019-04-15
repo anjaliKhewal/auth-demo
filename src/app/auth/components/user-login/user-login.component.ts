0b01138431b4d2bc1b2d44873ad7d8a7f1c83108
Anjali Mac <anjali_mac@Anjalis-MacBook-Pro.local>
2019-04-14 23:12:07 -0400

    updated UI

--- src/app/auth/components/user-login/user-login.component.ts
+++ src/app/auth/components/user-login/user-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpRequest,HttpEvent,HttpEventType } from '@angular/common/http';
+import { LoginServiceService, LoginInterface } from 'src/app/services/login-service.service';
 
 @Component({
   selector: 'app-user-login',
@@ -14,16 +15,32 @@ export class UserLoginComponent implements OnInit {
   errorMessage: string;
   url: string ='http://127.0.0.1:8080/login/all/user';
   userObservable : Observable<[]>;
+  loginRes:LoginInterface;
+  res:string;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private loginService:LoginServiceService) { }
 
   ngOnInit() {
     
-    this.httpClient.get(this.url).subscribe(data => {
-      console.log(data);
-    });
+   // this.httpClient.get(this.url).subscribe(data => {
+     // console.log(data);
+    //});
 
     }
+
+    search(username,password){
+      this.loginService.login(username,password).subscribe(loginRes =>{
+        this.loginRes = loginRes
+        console.log(loginRes);
+      });
+    } 
+
+    /*search(username,password){
+      this.loginService.login().subscribe(loginRes =>{
+        this.res = loginRes.toString();
+        console.log(this.res);
+      });
+    }*/
     
   }
 
